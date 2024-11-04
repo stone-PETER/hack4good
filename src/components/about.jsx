215d67082a384c42f4cc2e81741c60156d8a997b
DATAPIRATE17 <124865981+DATAPIRATE17@users.noreply.github.com>
2024-11-04 10:12:31 +0530

    commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,7 +13,7 @@ export const About = (props) => {
             <div className="about-text">
               <h2>About Hack4Good </h2>
               <p>
-              Hack4Good, an innovative platform where coding meets compassion and creativity meets community. Flagship Hackathon event of the IEEE Signal Processing Society (SPS) Kerala Chapter,Powered by IEEE Humanitarian Technologies Board ,the satellite event for ICASSP 2025 organized by the IEEE SPS Kerala Chapter, Hack4Good is a dynamic Three phased Hackathon aimed at leveraging technology to address real-world challenges and advance the United Nations' Sustainable Development Goals (SDGs). At Hack4Good, we believe in the transformative power of technology to drive positive change.Our mission is to provide a space where developers of all levels - from seasoned professionals to budding enthusiasts - can come together to collaborate, learn, and innovate.The aim is to find good effective solutions for problems that are being obstacles to Sustainability. Hackathon is divided into three phases,with first two phases being online and Final phase will be a 24 hour Offline Hackathon. Top deserving 40 teams are to be selected into the Final phase and will be provided with Mentors for mentorship and guidance.
+              Hack4Good, an innovative platform where coding meets compassion and creativity meets community. Flagship Hackathon event of the IEEE Signal Processing Society (SPS) Kerala Chapter,Powered by IEEE Humanitarian Technologies Board ,the satellite event for ICASSP 2025 organized by the IEEE SPS Kerala Chapter, Hack4Good is a dynamic Three phased Hackathon aimed at leveraging technology to address real-world challenges and advance the United Nations' Sustainable Development Goals (SDGs). At Hack4Good, we believe in the transformative power of technology to drive positive change. Our mission is to provide a space where developers of all levels - from seasoned professionals to budding enthusiasts - can come together to collaborate, learn, and innovate. The aim is to find good effective solutions for problems that are being obstacles to Sustainability. Hackathon is divided into three phases,with first two phases being online and Final phase will be a 24 hour Offline Hackathon. Top deserving 40 teams are to be selected into the Final phase and will be provided with Mentors for mentorship and guidance.
               </p>
             </div>
           </div>
@@ -38,7 +38,7 @@ export const About = (props) => {
                 The IEEE Humanitarian Technologies Board (HTB) inspires and
                 empowers IEEE volunteers around the world carrying out and
                 supporting impactful humanitarian technology activities at the
-                local level.The IEEE HTB mission is to support impactful and
+                local level. The IEEE HTB mission is to support impactful and
                 ethically informed volunteer-led initiatives, programs and
                 projects, and mutually beneficial partnerships, as well as to
                 inform policy formulation that harness technology and innovation
