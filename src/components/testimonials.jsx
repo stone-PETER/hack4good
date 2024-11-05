4d189b4f9685d6b31cfe3e51031e41b26b895ee4
Alan Antony <voa.alan@gmail.com>
2024-11-05 16:29:23 +0530

    registration comment

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -11,30 +11,39 @@ export const Testimonials = (props) => {
           <h2>Problem Statements</h2>
         </div>
         {/* Group by unique SDG values */}
-        {Array.from(new Set(props.data?.map(d => d.SDG) || [])).map(sdg => (
+        {Array.from(new Set(props.data?.map((d) => d.SDG) || [])).map((sdg) => (
           <div key={sdg} className="row">
             <div className="col-md-12">
               <h3>{sdg}</h3>
               <div className="row">
                 {/* Group by 'no' attribute within each SDG */}
-                {Array.from(new Set(props.data?.filter(d => d.SDG === sdg).map(d => d.no) || [])).map(no => {
+                {Array.from(
+                  new Set(
+                    props.data?.filter((d) => d.SDG === sdg).map((d) => d.no) ||
+                      []
+                  )
+                ).map((no) => {
                   const currentNumber = counter++;
                   return (
                     <div key={`${sdg}-${no}`} className="col-md-4">
-                    <a
+                      {/* <a
                             href="https://tinyurl.com/Hack4Good-Registration"
                             className="btn btn-custom btn-lg testimonial-btn page-scroll"
                           >
                             Register Now
-                          </a>{" "}
+                          </a>{" "} */}
                       <div className="testimonial">
                         <div className="testimonial-content">
-                          <div className="testimonial-number">Problem Statement {currentNumber}</div>
+                          <div className="testimonial-number">
+                            Problem Statement {currentNumber}
+                          </div>
                           {props.data
-                            ?.filter(d => d.SDG === sdg && d.no === no)
+                            ?.filter((d) => d.SDG === sdg && d.no === no)
                             .map((d, i) => (
                               <div key={i}>
-                                <div className="testimonial-meta">{d.title}</div>
+                                <div className="testimonial-meta">
+                                  {d.title}
+                                </div>
                                 {d.text && <p>{d.text}</p>}
                               </div>
                             ))}
