424af8d274d2ce80fe1da7be65f3cf6a096a40b8
DATAPIRATE17 <124865981+DATAPIRATE17@users.noreply.github.com>
2024-11-05 19:28:03 +0530

    commit

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -26,12 +26,12 @@ export const Testimonials = (props) => {
                   const currentNumber = counter++;
                   return (
                     <div key={`${sdg}-${no}`} className="col-md-4">
-                      {/* <a
+                      <a
                             href="https://tinyurl.com/Hack4Good-Registration"
                             className="btn btn-custom btn-lg testimonial-btn page-scroll"
                           >
                             Register Now
-                          </a>{" "} */}
+                          </a>{" "}
                       <div className="testimonial">
                         <div className="testimonial-content">
                           <div className="testimonial-number">
